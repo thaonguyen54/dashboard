ddccdfd974b40f4f8514021362fe416db4d77ef8
k1v4z <addnguyen32gmail.com>
2024-11-08 17:22:23 +0700

    style: Adjust components

--- app/components/Login/Option.tsx
+++ app/components/Login/Option.tsx
@@ -6,12 +6,12 @@ const Option = () => {
   return (
     <div className="flex justify-between items-center mt-4">
       <div className="flex items-center gap-2 font-medium text-base">
-        <Checkbox className="bg-primary-light border-primary-grey-light dark:bg-primary-dark dark:border-secondary-dark"/>
+        <Checkbox color="#ff0" className="bg-primary-light border-primary-grey-light dark:bg-primary-dark dark:border-secondary-dark"/>
         <p className="text-secondary-grey-light dark:text-secondary-grey-dark">
           Remember Me
         </p>
       </div>
-      <p className="font-bold text-base bg-gradient-to-br from-[#82DBF7] to-[#B6F09C] bg-clip-text text-transparent cursor-pointer hover:underline">
+      <p className="font-bold text-base bg-gradient-to-br from-custom-aqua to-custom-green bg-clip-text text-transparent cursor-pointer hover:underline">
         Forgot Password?
       </p>
     </div>
