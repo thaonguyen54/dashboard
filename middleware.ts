7a090a6e8e825579ccf49ed3c1bc471f1e518f88
k1v4z <addnguyen32gmail.com>
2024-11-12 11:54:00 +0700

    feat: Define locale middleware for language switch

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,41 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+const initMiddleware = createMiddleware(routing);
+
+const localeMiddleware = (request: NextRequest) => {
+    const pathname = request.nextUrl.pathname;
+    const languageSwitch = request.nextUrl.searchParams.get('lang-switch');
+    const locale = request.cookies.get('NEXT_LOCALE')?.value || '';
+
+    if (languageSwitch == null) {
+        return NextResponse.next();
+    }
+
+    if (locale != languageSwitch || languageSwitch == null) {
+        const newPathname = pathname.split('/').filter(Boolean).map(segment => segment === locale ? languageSwitch : segment).join('/')
+        const redirectURL = new URL(`/${newPathname}`, request.nextUrl.origin)
+        return NextResponse.redirect(redirectURL);
+    }
+
+    return NextResponse.next();
+}
+
+const middleware = (request: NextRequest) => {
+    const authResponse = localeMiddleware(request);
+    if (authResponse.status == 307) {
+        return authResponse;
+    }
+
+    return initMiddleware(request);
+}
+
+export default middleware;
 
 export const config = {
     // Match only internationalized pathnames
-    matcher: ['/login','/register', '/(en|vi)/:path*']
+    matcher: ['/login', '/register', '/(en|vi)/:path*']
 };
