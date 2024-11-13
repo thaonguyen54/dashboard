4c75fc8ee4d379e9825f807b6511dc306b23dd27
k1v4z <addnguyen32gmail.com>
2024-11-13 13:48:38 +0700

    feat: Add locale to return

--- i18n/request.ts
+++ i18n/request.ts
@@ -3,12 +3,13 @@ import { routing } from '../i18n/routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
     let locale = await requestLocale;
-
-    if (!locale || !routing.locales.includes(locale as any)) {
+    
+    if (!locale || !routing.locales.includes(locale as any)) {    
         locale = routing.defaultLocale;
     }
 
     return {
+        locale,
         messages: (await import(`../messages/${locale}.json`)).default
     };
 });
