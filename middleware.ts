205fb20c284dc8f4dcd107b0e00053b1990f406b
k1v4z <addnguyen32gmail.com>
2024-11-13 13:51:07 +0700

    feat: Adjust locale middleware

--- middleware.ts
+++ middleware.ts
@@ -3,39 +3,40 @@ import { routing } from './i18n/routing';
 
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import type { Locale } from './types/globals/Locale';
 
-const initMiddleware = createMiddleware(routing);
+const DEFAULT_LOCALE = 'en';
+const LOCALES = ['en', 'vi'];
 
 const localeMiddleware = (request: NextRequest) => {
     const pathname = request.nextUrl.pathname;
-    const languageSwitch = request.nextUrl.searchParams.get('lang-switch');
-    const locale = request.cookies.get('NEXT_LOCALE')?.value || '';
+    const segments = pathname.split('/');
 
-    if (languageSwitch == null) {
+    if (LOCALES.includes(segments[1])) {
         return NextResponse.next();
+    } else {
+        segments[1] = DEFAULT_LOCALE;
+        const newPathname = segments.join('/');
+        const urlString =  new URL(`${request.nextUrl.origin}${newPathname}`).toString();
+        return NextResponse.redirect(urlString);
     }
-
-    if (locale != languageSwitch || languageSwitch == null) {
-        const newPathname = pathname.split('/').filter(Boolean).map(segment => segment === locale ? languageSwitch : segment).join('/')
-        const redirectURL = new URL(`/${newPathname}`, request.nextUrl.origin)
-        return NextResponse.redirect(redirectURL);
-    }
-
-    return NextResponse.next();
 }
 
 const middleware = (request: NextRequest) => {
-    const authResponse = localeMiddleware(request);
-    if (authResponse.status == 307) {
-        return authResponse;
+    const locale = (request.cookies.get('NEXT_LOCALE')?.value) || DEFAULT_LOCALE;
+    routing.defaultLocale = locale as Locale;
+
+    const localeResponse = localeMiddleware(request);
+    if (localeResponse.status == 307) {
+        return localeResponse;
     }
 
-    return initMiddleware(request);
+    const handleI18nRouting = createMiddleware(routing);
+    return handleI18nRouting(request);
 }
 
 export default middleware;
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/login', '/register', '/(en|vi)/:path*']
+    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|sitemap.xml|robots.txt).*)']
 };
