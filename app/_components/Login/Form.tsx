f9e521f228f5b99e46f890fcb9a5c0026b13ac1a
k1v4z <addnguyen32gmail.com>
2024-11-14 17:59:08 +0700

    feat: Implement authentication

--- app/_components/Login/Form.tsx
+++ app/_components/Login/Form.tsx
@@ -1,11 +1,16 @@
+"use client"
 import React from 'react';
 import Link from 'next/link';
 import { useTranslations } from "next-intl";
+import { useRouter } from 'next/navigation';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Heading from './Heading';
 import InputFields from './InputFields';
 import Option from './Option';
-
+import useAuthStore from '@/stores/authStore';
 interface FormProps {
   type: string
 }
@@ -25,17 +30,61 @@ const FORM_CONTENT = {
   }
 }
 
-const Form = ({ type }: FormProps) => {
-  const t = useTranslations("Index");
 
+const Form = ({ type }: FormProps) => {
+  
+  const t = useTranslations("AUTH");
+  
   const { BUTTON_TEXT, BOTTOM_TEXT, LINK, SPAN_BOTTOM_TEXT } = FORM_CONTENT[type as keyof typeof FORM_CONTENT];
+  const authStore = useAuthStore();
+  const router = useRouter();
+
+  const handleAuth = async () => {
+    const authData = authStore.authData;
+    
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/local`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        identifier: authData.email,
+        password: authData.password, 
+      }),
+    })
+
+    const { jwt } = await response.json();
+    if(!jwt) {
+      toast.error('Email or password is incorrect');
+      return;
+    }
+
+    //clear data in store after Login successful
+    authStore.clearStore();
+    
+    const setToken = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/set-token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        accessToken: jwt
+      })
+    })
+
+    if (setToken.status === 200) {
+      router.push('/dashboard');
+    }
+  }
 
   return (
     <div className="w-[45%]">
           <Heading />
           <InputFields />
           <Option />
-          <button className="w-full bg-custom-green text-black font-semibold py-2 mt-6 rounded-lg transition duration-300">
+          <button 
+            className="w-full bg-custom-green text-black font-semibold py-2 mt-6 rounded-lg transition duration-300"
+            onClick={() => handleAuth()}>
             {t(BUTTON_TEXT)}
           </button>
           {
@@ -48,6 +97,7 @@ const Form = ({ type }: FormProps) => {
               </Link>
             </div>
           }
+          <ToastContainer />
     </div>
   )
 }
