007b93a456ab998ca8bcb0e3494dd96f4c573c32
k1v4z <addnguyen32gmail.com>
2024-11-15 10:02:41 +0700

    refactor: replace jwt decode function

--- app/utils/validateJWT.ts
+++ app/utils/validateJWT.ts
@@ -1,12 +1,23 @@
-import { jwtDecode } from 'jwt-decode';
+
+const base64ToObject = (s: string) => JSON.parse(atob(s));
+
+const decodeJWT = (token: string) => {
+  const [header, payload, signature] = token.split(".");
+
+  return {
+    header: base64ToObject(header),
+    payload: base64ToObject(payload),
+    signature,
+  };
+};
 
 export const isTokenExpired = (token: string) => {
-    if (!token) return true;
-    try {
-        const decodedToken = jwtDecode(token);
-        const currentTime = Date.now() / 1000;
-        return decodedToken.exp ? decodedToken.exp < currentTime : true;
-    } catch (error) {
-        return true;
-    }
+  if (!token) return true;
+  try {
+    const decodedToken = decodeJWT(token);
+    const currentTime = Date.now() / 1000;
+    return decodedToken.payload.exp ? decodedToken.payload.exp < currentTime : true;
+  } catch (error) {
+    return true;
+  }
 };
