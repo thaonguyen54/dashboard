902ee3c5f70833f5b6515ae7ec28a0319cb94ff2
k1v4z <addnguyen32gmail.com>
2024-11-15 16:35:45 +0700

    feat: customize input fields components

--- components/ui/input-field.tsx
+++ components/ui/input-field.tsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React, { forwardRef } from "react";
 
 interface InputFieldProps {
   icon: React.ReactNode;
@@ -9,24 +9,30 @@ interface InputFieldProps {
   setInputValue?: (value: string) => void;
 }
 
-const InputField = ({ icon, placeholder, type, setInputValue }: InputFieldProps) => {
-  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if(setInputValue) {
-      setInputValue(e.target.value);
-    }
-  }
+const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
+  ({ icon, placeholder, type, setInputValue, ...props}, ref) => {
+    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (setInputValue) {
+        setInputValue(e.target.value);
+      }
+    };
 
-  return (
-    <div className="relative flex items-center">
-      <span className="absolute left-3 text-secondary-grey-dark">{icon}</span>
-      <input
-        className="w-full text-main bg-input border-input  text-base placeholder-secondary-grey-dark  p-2 pl-10 rounded-lg border focus:outline-none focus:border-2 focus:border-custom-green"
-        type={type}
-        onChange={handleChangeInput}
-        placeholder={placeholder}
-      />
-    </div>
-  );
-};
+    return (
+      <div className="relative flex items-center">
+        <span className="absolute left-3 text-secondary-grey-dark">{icon}</span>
+        <input
+          className="w-full text-main bg-input border-input text-base placeholder-secondary-grey-dark p-2 pl-10 rounded-lg border focus:outline-none focus:border-2 focus:border-custom-green"
+          type={type}
+          onChange={handleChangeInput}
+          placeholder={placeholder}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+InputField.displayName = "InputField";
 
 export default InputField;
