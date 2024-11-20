892632624ea0bc0bb0b0ec8ce08e0a50f0cb60dc
k1v4z <addnguyen32gmail.com>
2024-11-20 17:54:27 +0700

    chore: Comment code initially

--- app/(withAuth)/[locale]/layout.tsx
+++ app/(withAuth)/[locale]/layout.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+
 import { isTokenExpired } from "@/app/utils/validateJWT";
+
+import {AbstractIntlMessages, NextIntlClientProvider, useMessages} from 'next-intl';
 
 const withAuthLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("at")?.value;  
+  // const cookieStore = cookies();
+  // const accessToken = cookieStore.get("at")?.value;  
 
-  if (!accessToken || isTokenExpired(accessToken)) {
-    redirect("/login"); // Redirect to login page if no access token
-  }
+  // if (!accessToken || isTokenExpired(accessToken)) {
+  //   redirect("/login"); // Redirect to login page if no access token
+  // }
+
+  const messages: AbstractIntlMessages = useMessages();
 
   return (
-    <div>{children}</div>
+    <div suppressHydrationWarning>
+      <NextIntlClientProvider messages={messages as AbstractIntlMessages}>
+          {children}
+      </NextIntlClientProvider>
+    </div>
   )
 };
 
