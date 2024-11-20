c04d45175249d2bda844fff13046d639857edb53
k1v4z <addnguyen32gmail.com>
2024-11-20 17:51:58 +0700

    feat: Relocate dashboard location and add Task component

---
+++ app/(withAuth)/[locale]/(withSideBar)/task/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Task = () => {
+  return (
+    <div>
+      <header className="flex justify-between items-center mb-4 mt-2 ml-4">
+        <h1 className="text-2xl font-bold">Task</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Task
