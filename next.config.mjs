127d3c6ee626dcc645fdb159751bc7c02045a297
k1v4z <addnguyen32gmail.com>
2024-11-21 17:59:11 +0700

    chore: Config image loading

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'engaging-dream-7f75d5f5db.media.strapiapp.com',
+                port: ''
+            }
+        ]
+    }
+}
 
 export default withNextIntl(nextConfig);
