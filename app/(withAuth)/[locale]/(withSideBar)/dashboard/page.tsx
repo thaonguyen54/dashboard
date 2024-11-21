f4189cf9c581492883cda6d749a60764cfc6f72c
k1v4z <addnguyen32gmail.com>
2024-11-21 10:32:33 +0700

    style: Relocate unnecessary data go out func component

--- app/(withAuth)/[locale]/(withSideBar)/dashboard/page.tsx
+++ app/(withAuth)/[locale]/(withSideBar)/dashboard/page.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import DashboardTable from './_components/table';
+import DashboardTable from "./_components/table";
+
+// Fake data
+const generateFakeData = (count: number) => {
+  return Array.from({ length: count }, (_, i) => ({
+    id: i + 1,
+    project: "Sisyphus",
+    domain: "sisyphus.com",
+    lastAssessed: `${16 + (i % 15)} Jan 2022`,
+    licenseStatus: "Active",
+    licenseUse: ["Customer data", "Admin"],
+  }));
+};
+
+const data = generateFakeData(10);
 
 const Dashboard = () => {
-  // Fake data
-  const generateFakeData = (count: number) => {
-    return Array.from({ length: count }, (_, i) => ({
-      id: i + 1,
-      project: 'Sisyphus',
-      domain: 'sisyphus.com',
-      lastAssessed: `${16 + (i % 15)} Jan 2022`,
-      licenseStatus: 'Active',
-      licenseUse: ['Customer data', 'Admin'],
-    }));
-  };
-
-  const data = generateFakeData(10);
-
   return (
-    <div className='w-full min-h-screen'>
+    <div className="w-full min-h-screen">
       <header className="flex justify-between items-center mb-4 mt-2 ml-4">
         <h1 className="text-2xl text-main font-bold">Dashboard</h1>
       </header>
