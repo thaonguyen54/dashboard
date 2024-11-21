ff0ae14fb3291f6119cdc55a0206c0dd875cb407
k1v4z <addnguyen32gmail.com>
2024-11-21 17:56:20 +0700

    feat: Implement multiple for dashboard UI

--- app/(withAuth)/[locale]/(withSideBar)/layout.tsx
+++ app/(withAuth)/[locale]/(withSideBar)/layout.tsx
@@ -1,28 +1,35 @@
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import React from 'react'
-import AppSideBar from './_components/sidebar';
-import DashboardHeading from './_components/header';
-import { cookies } from 'next/headers';
+import { SidebarProvider } from "@/components/ui/sidebar";
+import React from "react";
+import AppSideBar from "./_components/sidebar";
+import DashboardHeading from "./_components/header";
+import { cookies } from "next/headers";
+import {
+  AbstractIntlMessages,
+  NextIntlClientProvider,
+  useMessages,
+} from "next-intl";
 
 const withNavLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
- const cookieStore = cookies()
-const defaultOpen = cookieStore.get("sidebar:state")?.value === "true"
-  
+  const cookieStore = cookies();
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
+  const messages: AbstractIntlMessages = useMessages();
   return (
-    <div className='bg-bg-dashboard'> 
+    <div className="bg-bg-dashboard">
       <SidebarProvider defaultOpen={defaultOpen}>
-        <AppSideBar/>
-        <div className='flex flex-col w-full min-h-screen'>
-          <DashboardHeading/>
-          {children}
+        <AppSideBar />
+        <div className="flex flex-col w-full min-h-screen">
+          <NextIntlClientProvider messages={messages as AbstractIntlMessages}>
+            <DashboardHeading />
+            {children}
+          </NextIntlClientProvider>
         </div>
       </SidebarProvider>
     </div>
-  )
+  );
 };
 
 export default withNavLayout;
